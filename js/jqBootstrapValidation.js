d9c62cfd619bfa5c581b0d3609004fdde1c4dfa5
sebdina <95606603+sebdina@users.noreply.github.com>
2022-04-12 16:38:33 +0400

    corrections page contact

--- js/jqBootstrapValidation.js
+++ js/jqBootstrapValidation.js
@@ -791,7 +791,7 @@
 				name: "Validemail",
 				type: "regex",
 				regex: "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\\.[A-Za-z]{2,10}",
-				message: "Not a valid email address<!-- data-validator-validemail-message to override -->"
+				message: "Adresse mail invalide<!-- data-validator-validemail-message to override -->"
 			},
 			passwordagain: {
 				name: "Passwordagain",
@@ -836,13 +836,13 @@
 			required: {
 				name: "Required",
 				type: "required",
-				message: "This is required<!-- data-validator-required-message to override -->"
+				message: "Veuillez remplir ce champ<!-- data-validator-required-message to override -->"
 			},
 			checkone: {
 				name: "Checkone",
 				type: "minchecked",
 				minchecked: 1,
-				message: "Check at least one option<!-- data-validation-checkone-message to override -->"
+				message: "Cochez au moins une option<!-- data-validation-checkone-message to override -->"
 			}
 		}
 	};
